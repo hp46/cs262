88a57b22178bef05c4513086bfbebe927d60501d
hp46 <hp46@maroon01.cs.calvin.edu>
2019-09-27 22:50:31 -0400

    Caculator

--- Homework1/app/src/main/java/edu/calvin/cs262/hp46/MainActivity.java
+++ Homework1/app/src/main/java/edu/calvin/cs262/hp46/MainActivity.java
@@ -10,40 +10,66 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     public String operator;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner mySpinner = (Spinner) findViewById(R.id.spinner);
-        String operationString = mySpinner.getSelectedItem().toString();
+        Spinner myspinner = findViewById(R.id.spinner);
+        if (myspinner != null){
+            myspinner.setOnItemSelectedListener(this);
+        }
 
-
-        ArrayAdapter<String> myAdaptor= new ArrayAdapter<String>(MainActivity.this,
-                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.names));
-        myAdaptor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mySpinner.setAdapter(myAdaptor);
-
-        mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                operator = adapterView.getItemAtPosition(i).toString();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        if(myspinner != null){
+            myspinner.setAdapter(adapter);
+        }
 
     }
-    public void calcuate(View view) {
+    //calucuate input
+    public void calculate(View view) {
+        
+        //variables
+        EditText input_1 = findViewById(R.id.input1);
+        EditText input_2 = findViewById(R.id.input2);
+
+        String input_1_string = input_1.getText().toString();
+        String input_2_string = input_2.getText().toString();
+
+        int result = 0;
+        int input_1_int = Integer.parseInt(input_1_string);
+        int input_2_int = Integer.parseInt(input_2_string);
+
+        if (operator.equals("+")){
+            result = input_1_int + input_2_int;
+        } else if (operator.equals("-")){
+            result = input_1_int - input_2_int;
+        } else  if (operator.equals("/")){
+            result = input_1_int / input_2_int;
+        } else  if (operator.equals("*")){
+            result = input_1_int * input_2_int;
+        }
+
+        //result
+        TextView textView = findViewById(R.id.result);
+        String result_string = Integer.toString(result);
+        textView.setText(result_string);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        operator = adapterView.getItemAtPosition(i).toString();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
